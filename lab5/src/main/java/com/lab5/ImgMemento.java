7b534f43b516b4075273db17820233adcc2fa243
Alexis-Goyette <algoyette.ag@gmail.com>
2024-04-04 14:23:40 -0400

    ImgMemento fait

--- lab5/src/main/java/com/lab5/ImgMemento.java
+++ lab5/src/main/java/com/lab5/ImgMemento.java
@@ -1,5 +1,17 @@
 package com.lab5;
 
+import javafx.scene.image.Image;
+
 public class ImgMemento {
-    
+
+    private Image etat;
+
+    public void SetEtat(Image i) {
+        etat = i;
+    }
+
+    public Image GetEtat() {
+        return etat;
+    }
+
 }
--- lab5/src/main/java/module-info.java
+++ lab5/src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.lab5 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     opens com.lab5 to javafx.fxml;
+
     exports com.lab5;
 }
